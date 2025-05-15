e9388293dd6af452623b02bc33111aa9f29b2301
DoubleRiichi <DoubleRiichi>
2025-05-15 11:48:17 +0200

    another issue most likely caused by the connection string using PDO

--- videgrenier/Core/Model.php
+++ videgrenier/Core/Model.php
@@ -23,7 +23,7 @@ abstract class Model
         static $db = null;
 
         if ($db === null) {
-            $dsn = 'mysql:host=' . Config::$DB_HOST . ":" . Config::$DB_PORT . ';dbname=' . Config::$DB_NAME . ';charset=utf8';
+            $dsn = 'mysql:host=' . Config::$DB_HOST . ";port=" . Config::$DB_PORT . ';dbname=' . Config::$DB_NAME . ';charset=utf8';
             $db = new PDO($dsn, Config::$DB_USER, Config::$DB_PASSWORD);
 
             // Throw an Exception when an error occurs
