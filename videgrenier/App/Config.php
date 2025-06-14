0b6308c0d9fee72595f2d2c799f4c68a8c69eb80
DoubleRiichi <DoubleRiichi>
2025-06-14 10:53:44 +0200

    fixed a glitch where DB_HOST was overwritten by ENV['DB_PORT'] in App/Config.php

--- videgrenier/App/Config.php
+++ videgrenier/App/Config.php
@@ -27,7 +27,7 @@ class Config
         $dotenv->safeLoad();
 
         self::$DB_HOST = $_ENV['DB_HOST'] ?? 'localhost';
-        self::$DB_HOST = $_ENV['DB_PORT'] ?? '3306';
+        self::$DB_PORT = $_ENV['DB_PORT'] ?? '3306';
         self::$DB_NAME = $_ENV['DB_NAME'] ?? 'videgrenierenligne-dev';
         self::$DB_USER = $_ENV['DB_USER'] ?? 'default_user';
         self::$DB_PASSWORD = $_ENV['DB_PASSWORD'] ?? 'default_password';
